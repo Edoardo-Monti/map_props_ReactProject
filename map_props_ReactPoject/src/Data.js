db588d85ab3d4217a008b7da5510830ca39d7cdd
Edoardo Monti <edoardo.monti.bo@gmail.com>
2024-03-28 13:27:02 +0100

    add description

--- map_props_ReactPoject/src/Data.js
+++ map_props_ReactPoject/src/Data.js
@@ -3,28 +3,25 @@ export default [
         title: "Monte Fuji",
         location: "Japan",
         googleMapsUrl: "",
-        startDate: "",
-        endDate: "",
-        description: "",
+        lastUpdate: "",
+        description: "Il Monte Fuji è un'icona del paesaggio giapponese, un vulcano attivo che si innalza con grazia e maestosità sopra la pianura circostante. La sua forma conica perfetta e la sua aura sacra lo rendono un simbolo di bellezza e spiritualità, attraendo visitatori da tutto il mondo per contemplare la sua imponenza e per cercare ispirazione nella sua quiete eterna.",
         imageUrl: "monte_fuji.jpeg"
     },
     {
         title: "Cascate del Niagara",
         location: "Japan",
         googleMapsUrl: "",
-        startDate: "",
-        endDate: "",
-        description: "",
+        lastUpdate: "",
+        description: "Le Cascate del Niagara sono una delle meraviglie naturali più iconiche del mondo, situate al confine tra Stati Uniti e Canada. Con la loro maestosità e il fragore dell'acqua che si riversa giù dalle scogliere, le cascate offrono uno spettacolo mozzafiato e una sensazione di potenza naturale incommensurabile.",
         imageUrl: "niagara.jpeg"
     },
     {
         title: "Emingway",
         location: "Japan",
         googleMapsUrl: "",
-        startDate: "",
-        endDate: "",
-        description: "",
-        imageUrl: ""
+        lastUpdate: "",
+        description: "Le Piramidi sono antiche strutture architettoniche che hanno resistito al passare dei millenni, testimonianze monumentali di civiltà antiche come quella egizia. Con la loro imponente presenza e il loro mistero, le piramidi incantano e affascinano, invitando a riflettere sulla grandezza e sulla maestria delle civiltà del passato.",
+        imageUrl: "piramidi.jpeg"
     },
    
 ]
--- map_props_ReactPoject/src/component/Card/Card.jsx
+++ map_props_ReactPoject/src/component/Card/Card.jsx
@@ -14,7 +14,7 @@ export default function Card(props){
                         <div className="col-md-8">
                             <div className="card-body">
                                 <h5 className="card-title">{props.title}</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p className="card-text description">{props.description}</p>
                                 <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
                             </div>
                         </div>
