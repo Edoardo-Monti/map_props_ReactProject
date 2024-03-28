2f0a955fc810b41a7aa695d8d00c83bfa5e8a47f
Edoardo Monti <edoardo.monti.bo@gmail.com>
2024-03-28 13:32:30 +0100

    dettagli inutili

--- map_props_ReactPoject/src/Data.js
+++ map_props_ReactPoject/src/Data.js
@@ -3,7 +3,7 @@ export default [
         title: "Monte Fuji",
         location: "Japan",
         googleMapsUrl: "",
-        lastUpdate: "",
+        lastUpdate: "3 month ago",
         description: "Il Monte Fuji è un'icona del paesaggio giapponese, un vulcano attivo che si innalza con grazia e maestosità sopra la pianura circostante. La sua forma conica perfetta e la sua aura sacra lo rendono un simbolo di bellezza e spiritualità, attraendo visitatori da tutto il mondo per contemplare la sua imponenza e per cercare ispirazione nella sua quiete eterna.",
         imageUrl: "monte_fuji.jpeg"
     },
@@ -11,7 +11,7 @@ export default [
         title: "Cascate del Niagara",
         location: "Japan",
         googleMapsUrl: "",
-        lastUpdate: "",
+        lastUpdate: "Yesterday",
         description: "Le Cascate del Niagara sono una delle meraviglie naturali più iconiche del mondo, situate al confine tra Stati Uniti e Canada. Con la loro maestosità e il fragore dell'acqua che si riversa giù dalle scogliere, le cascate offrono uno spettacolo mozzafiato e una sensazione di potenza naturale incommensurabile.",
         imageUrl: "niagara.jpeg"
     },
@@ -19,7 +19,7 @@ export default [
         title: "Emingway",
         location: "Japan",
         googleMapsUrl: "",
-        lastUpdate: "",
+        lastUpdate: "1 week ago",
         description: "Le Piramidi sono antiche strutture architettoniche che hanno resistito al passare dei millenni, testimonianze monumentali di civiltà antiche come quella egizia. Con la loro imponente presenza e il loro mistero, le piramidi incantano e affascinano, invitando a riflettere sulla grandezza e sulla maestria delle civiltà del passato.",
         imageUrl: "piramidi.jpeg"
     },
--- map_props_ReactPoject/src/component/Card/Card.jsx
+++ map_props_ReactPoject/src/component/Card/Card.jsx
@@ -15,7 +15,7 @@ export default function Card(props){
                             <div className="card-body">
                                 <h5 className="card-title">{props.title}</h5>
                                 <p className="card-text description">{props.description}</p>
-                                <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
+                                <p className="card-text"><small className="text-body-secondary">{props.lastUpdate}</small></p>
                             </div>
                         </div>
                     </div>
